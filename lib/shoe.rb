576f605e3446a1b7a6cc4c42fee59b1bb9155697
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-14 04:51:13 +0000

    Automatically backed up by Learn

---
+++ lib/shoe.rb
@@ -0,0 +1,14 @@
+class Shoe 
+  attr_accessor :brand, :color, :size, :material, :condition 
+
+  def initialize(brand)
+    @brand = brand 
+  end
+
+  def cobble
+    puts "The show has been repaired."
+    @condition = New 
+  end 
+
+end
+  
